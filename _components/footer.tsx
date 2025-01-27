b3f40fc2091d2b9e377e587bff6266a034ed0dd4
Paul Fischer <me@paulmfischer.dev>
2025-01-27 16:12:57 -0600

    update lume (#6)

--- _components/footer.tsx
+++ _components/footer.tsx
@@ -7,7 +7,7 @@ export default (data: Lume.Data, helpers: Lume.Helpers) => {
       <div className="flex gap-2 items-center ml-3">
         <span className="w-6 h-6 lg:w-3 lg:h-3"><FontAwesomeIcon icon={faCopyright} /></span>2025<a href={data.github} className="text-blue-600 dark:text-blue-400">{data.author}</a>
         <span>|</span>
-        Built using<a href="https://lume.land/" className="text-blu-600 dark:text-blue-400">Lume</a>
+        Built using<a href="https://lume.land/" className="text-blue-600 dark:text-blue-400">Lume</a>
       </div>
     </footer>
   </>
