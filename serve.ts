afc1a6c25648a4d864024c2c09810fd7e7032cca
Paul Fischer <me@paulmfischer.dev>
2025-03-25 12:12:41 -0500

    adding logger middle-ware (#11)

--- serve.ts
+++ serve.ts
@@ -1,10 +1,12 @@
 import Server from 'lume/core/server.ts';
+import logger from "lume/middlewares/logger.ts";
 
 const server = new Server({
   port: 8000,
   root: `${Deno.cwd()}/_site`,
 });
 
+server.use(logger());
 server.start();
 
 console.log("Listening on http://localhost:8000");
