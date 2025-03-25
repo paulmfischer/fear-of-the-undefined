d354590406812b2a3a1a4de886d0904c67029c0b
Paul Fischer <me@paulmfischer.dev>
2025-03-25 12:38:11 -0500

    Revert "adding logger middle-ware" (#12)
    Revert "adding logger middle-ware (#11)"
    
    This reverts commit afc1a6c25648a4d864024c2c09810fd7e7032cca.

--- serve.ts
+++ serve.ts
@@ -1,12 +1,10 @@
 import Server from 'lume/core/server.ts';
-import logger from "lume/middlewares/logger.ts";
 
 const server = new Server({
   port: 8000,
   root: `${Deno.cwd()}/_site`,
 });
 
-server.use(logger());
 server.start();
 
 console.log("Listening on http://localhost:8000");
