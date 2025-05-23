ca19a284ddb08fd7667d67b5647e44e97e6ca036
Paul Fischer <me@paulmfischer.dev>
2025-05-23 15:07:00 -0500

    fixed header on mobile (#29)

--- _components/header.tsx
+++ _components/header.tsx
@@ -1,15 +1,19 @@
 export default ({ comp, ...data }: Lume.Data, helpers: Lume.Helpers) => {
   return <>
-    <div className="min-h-28 lg:min-h-0 flex flex-col lg:flex-row p-4 lg:p-2 bg-zinc-300 dark:bg-zinc-700 justify-between lg:justify-end gap-4">
-      <div className="lg:hidden flex gap-6 mt-3">
-        <button id="display-navigation" className="cursor-pointer dark:text-slate-400">
-          <comp.icon iconName="menu-2" className="w-12 h-12" />
-        </button>
-        <a href="/" className="m-auto">
-          <h2 className="text-5xl dark:text-slate-400">{data.siteTitle}</h2>
-        </a>
+    <div className="fixed w-full lg:relative flex flex-col lg:flex-row gap-5 justify-between lg:justify-end min-h-28 lg:min-h-0 p-4 lg:p-2 bg-zinc-300 dark:bg-zinc-700">
+      <div className="lg:hidden">
+        <div className="flex flex-col gap-4 justify-between">
+          <div className="flex gap-6 mt-3">
+            <button id="display-navigation" className="cursor-pointer dark:text-slate-400">
+              <comp.icon iconName="menu-2" className="w-12 h-12" />
+            </button>
+            <a href="/" className="m-auto">
+              <h2 className="text-5xl dark:text-slate-400">{data.siteTitle}</h2>
+            </a>
+          </div>
+        </div>
       </div>
-      <div id="search" className="lg:min-w-0"></div>
+        <div id="search" className="lg:min-w-0"></div>
     </div>
-  </>; 
+  </>;
 }
--- _includes/layouts/main.tsx
+++ _includes/layouts/main.tsx
@@ -11,7 +11,7 @@ export default ({ title, children, comp }: Lume.Data, helpers: Lume.Helpers) => 
         <link rel="alternate" type="application/rss+xml" title={`RSS Feed for ${siteOptions.baseUrl}`} href="/posts.rss" />
       </head>
       <body className="text-5xl lg:text-lg text-slate-900 bg-zinc-200 dark:text-slate-300 dark:bg-zinc-800">
-        <div className="max-w-8xl mx-auto px-4 sm:px-6 md:px-8">
+        <div className="max-w-8xl mx-auto lg:px-8">
           <div className="min-h-screen flex">
             <div id="sidebar" className="max-w-lg hidden lg:flex flex-col fixed inset-0 h-full lg:max-w-64 bg-zinc-200 dark:bg-zinc-800 z-50">
                 <button id="close-sidebar" className="absolute top-10 right-10 lg:hidden">
@@ -25,7 +25,7 @@ export default ({ title, children, comp }: Lume.Data, helpers: Lume.Helpers) => 
             </div>
             <div id="main" className="max-w-full grow flex flex-col lg:pl-56 bg-zinc-100 dark:bg-zinc-900">
               <comp.Header></comp.Header>
-              <div id="content" className="ml-10 m-5 mr-12 leading-14 lg:leading-7" data-pagefind-body>
+              <div id="content" className="ml-10 m-5 mr-12 pt-46 lg:pt-0 leading-14 lg:leading-7 text-4xl lg:text-lg" data-pagefind-body>
                 {children}
               </div>
               <div className="mt-auto">
